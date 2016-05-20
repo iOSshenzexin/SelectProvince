15ca4c581a9b21277058bed4b52074e3e1360252
iOSshenzexin <1045250128@qq.com>
2016-05-20 17:01:13 +0800

    Initial Commit

---
+++ 省市区选择/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  省市区选择
+//
+//  Created by 杨晓婧 on 16/5/20.
+//  Copyright © 2016年 QingDaoShangTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
